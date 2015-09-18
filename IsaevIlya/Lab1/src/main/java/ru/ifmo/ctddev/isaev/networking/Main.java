e9202ba007e0e284a9a0f1cf378907fe19058a9f
Ilya Isaev <isaev@solanteq.com>
2015-09-18 22:39:08 +0300

    jar packaging ok minor

--- IsaevIlya/Lab1/src/main/java/ru/ifmo/ctddev/isaev/networking/Main.java
+++ IsaevIlya/Lab1/src/main/java/ru/ifmo/ctddev/isaev/networking/Main.java
@@ -12,17 +12,23 @@ import java.util.concurrent.Executors;
 public class Main {
 
     public static final int PACKET_LENGTH = 128;
+    public static final int SLEEP_TIME = 5000;
     public static final Map<String, BroadcasterInfo> broadcasters = new TreeMap<>(String::compareTo);
     public static final Map<String, Message> pendingMessages = new HashMap<>();
     private static final Executor executor = Executors.newFixedThreadPool(3);
-    public static int PORT = 4445;
+    public static int SENDER_PORT = 4445;
+    public static int RECEIVER_PORT;
     public static String HOSTNAME;
 
     public static void main(String[] args) {
-        if (args.length != 2) {
-            System.out.println("Expected two arguments: <hostname> <port>");
+        if (args.length < 1) {
+            System.out.println("Expected two arguments: <hostname> [<port>]?");
+            return;
         }
-        PORT = Integer.parseInt(args[1]);
+        if (args.length > 1) {
+            SENDER_PORT = Integer.parseInt(args[1]);
+        }
+        RECEIVER_PORT = SENDER_PORT + 1;
         HOSTNAME = args[0];
         executor.execute(new Broadcaster());
         executor.execute(new Receiver());
