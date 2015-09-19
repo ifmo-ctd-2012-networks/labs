f7727d87e5a540a97c089b76742cfa617605d782
Ilya Isaev <isaev@solanteq.com>
2015-09-19 13:39:30 +0300

    delay argument support

--- IsaevIlya/Lab1/src/main/java/ru/ifmo/ctddev/isaev/networking/Main.java
+++ IsaevIlya/Lab1/src/main/java/ru/ifmo/ctddev/isaev/networking/Main.java
@@ -12,20 +12,23 @@ import java.util.concurrent.Executors;
 public class Main {
 
     public static final int PACKET_LENGTH = 128;
-    public static final int SLEEP_TIME = 5000;
     public static final Map<Long, BroadcasterInfo> broadcasters = new TreeMap<>(Long::compareTo);
     public static final Map<Long, Message> pendingMessages = new HashMap<>();
     private static final Executor executor = Executors.newFixedThreadPool(3);
+    public static int SLEEP_TIME = 5000;
     public static int PORT = 4445;
     public static String HOSTNAME;
 
     public static void main(String[] args) {
-        if (args.length < 1) {
-            System.out.println("Expected two arguments: <hostname> [<port>]?");
+        if (args.length < 2) {
+            System.out.println("Expected three arguments: <hostname> [port]? [timeout]?");
             return;
         }
         if (args.length > 1) {
             PORT = Integer.parseInt(args[1]);
+        }
+        if (args.length > 2) {
+            SLEEP_TIME = Integer.parseInt(args[2]);
         }
         HOSTNAME = args[0];
         executor.execute(new Broadcaster());
