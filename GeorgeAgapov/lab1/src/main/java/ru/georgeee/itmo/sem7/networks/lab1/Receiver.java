99a88e1d3dae0a751a498d075b9c0737e6f23cad
georgeee <george.agapov@gmail.com>
2015-09-19 13:25:23 +0300

    Implementation by George Agapov

---
+++ GeorgeAgapov/lab1/src/main/java/ru/georgeee/itmo/sem7/networks/lab1/Receiver.java
@@ -0,0 +1,46 @@
+package ru.georgeee.itmo.sem7.networks.lab1;
+
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.util.Map;
+
+
+@Component
+public class Receiver {
+    private final static Logger log = LoggerFactory.getLogger(Receiver.class);
+
+    @Autowired
+    private Settings settings;
+
+    public void run(Map<Long, Pair<Long, Message>> lastReceived) {
+        try {
+            DatagramSocket datagramSocket = new DatagramSocket(settings.getPort());
+            while (true) {
+                if (Thread.interrupted()) {
+                    Thread.currentThread().interrupt();
+                    break;
+                }
+
+                byte[] buf = new byte[4096];
+                DatagramPacket packet = new DatagramPacket(buf, 4096);
+                datagramSocket.receive(packet);
+                Message msg = Message.readFromBytes(packet.getData());
+                log.debug("Received message: {}", msg);
+
+                lastReceived.put(msg.getMacAddress(), new ImmutablePair<>(System.currentTimeMillis(), msg));
+            }
+        } catch (IOException e) {
+            log.info("Caught exception", e);
+        }
+    }
+
+}
+
