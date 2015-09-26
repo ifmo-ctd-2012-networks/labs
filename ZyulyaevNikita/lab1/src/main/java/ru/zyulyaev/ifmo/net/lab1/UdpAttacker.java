a202215344bbc4f6f9f82a154e5a727f5a0683c5
zyulyaev <nikita@zyulyaev.ru>
2015-09-26 14:46:29 +0300

    zyulyaev: better attacker

--- ZyulyaevNikita/lab1/src/main/java/ru/zyulyaev/ifmo/net/lab1/UdpAttacker.java
+++ ZyulyaevNikita/lab1/src/main/java/ru/zyulyaev/ifmo/net/lab1/UdpAttacker.java
@@ -6,6 +6,7 @@ import ru.zyulyaev.ifmo.net.lab1.components.BackgroundComponent;
 import ru.zyulyaev.ifmo.net.lab1.components.ComponentInitializationException;
 
 import java.io.IOException;
+import java.math.BigInteger;
 import java.net.*;
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -25,26 +26,34 @@ public class UdpAttacker extends BackgroundComponent {
     protected void init() throws ComponentInitializationException {
     }
 
-    private static byte[] randomData() {
+    private static DatagramPacket randomData() {
         int length = ThreadLocalRandom.current().nextInt(1000);
         byte[] data = new byte[length];
         ThreadLocalRandom.current().nextBytes(data);
-        return data;
+        return new DatagramPacket(data, data.length);
+    }
+
+    private static DatagramPacket randomValidData() {
+        byte[] mac = new byte[6];
+        ThreadLocalRandom.current().nextBytes(mac);
+        String host = new BigInteger(1000, ThreadLocalRandom.current()).toString(36);
+        return new UdpMessage(mac, host, ThreadLocalRandom.current().nextInt()).toPacket();
     }
 
     @Override
     protected void doInBackground() {
+        boolean valid = false;
         while (!Thread.interrupted()) {
             try (DatagramSocket socket = new DatagramSocket()) {
                 socket.setBroadcast(true);
+                socket.setReuseAddress(true);
                 NetworkInterface networkInterface = NetworkInterface.getByInetAddress(InetAddress.getLocalHost());
-                byte[] data = randomData();
-
+                DatagramPacket packet = (valid = !valid) ? randomValidData() : randomData();
                 for (InterfaceAddress interfaceAddress : networkInterface.getInterfaceAddresses()) {
                     InetAddress broadcast = interfaceAddress.getBroadcast();
                     if (broadcast == null)
                         continue;
-                    DatagramPacket packet = new DatagramPacket(data, data.length, new InetSocketAddress(broadcast, port));
+                    packet.setSocketAddress(new InetSocketAddress(broadcast, port));
                     socket.send(packet);
                 }
             } catch (IOException e) {
