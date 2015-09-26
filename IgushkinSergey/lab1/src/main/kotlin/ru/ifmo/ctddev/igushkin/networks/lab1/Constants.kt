8ca642951df0e519c5eb0c70aaa6ee9a492e36cb
Sergey Igushkin <hotkeytlt@gmail.com>
2015-09-26 12:56:05 +0300

    Improvements for lab1.

--- IgushkinSergey/lab1/src/main/kotlin/ru/ifmo/ctddev/igushkin/networks/lab1/Constants.kt
+++ IgushkinSergey/lab1/src/main/kotlin/ru/ifmo/ctddev/igushkin/networks/lab1/Constants.kt
@@ -1,9 +1,9 @@
 package ru.ifmo.ctddev.igushkin.networks.lab1
 
-val DEFAULT_PORT = 12345
+val DEFAULT_PORT = 1234
 val DEFAULT_PERIOD = 5000
 val BROADCAST_ADDRESS = "255.255.255.255"
-val PACKET_SIZE = 256
+val PACKET_SIZE = 1400
 
 val MISSED_TICKS_THRESHOLD = 5
 
