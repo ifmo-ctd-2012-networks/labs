d879d5041b191225889227d521431163252bde1b
georgeee <george.agapov@gmail.com>
2015-10-14 01:12:29 +0300

    [GeorgeAgapov] moved to separate repository

--- GeorgeAgapov/lab2/src/main/java/ru/ifmo/ctd/year2012/sem7/networks/lab2/jitterbug/Data.java
+++
@@ -1,9 +0,0 @@
-package ru.ifmo.ctd.year2012.sem7.networks.lab2.jitterbug;
-
-public interface Data<T extends Data<T>> extends Comparable<T> {
-    byte[] getBytes();
-
-    T next();
-
-    T readFromBytes(byte[] bytes);
-}
