40fa6fe1c225e898a06bf6d3b5ac981743470e2d
Nickolay Polyarniy <PolarNick239@gmail.com>
2015-10-25 23:00:14 +0300

    install_and_run script fixed (-y in apt-get install)

--- PolyarniyNikolay/Lab2/install_and_run.sh
+++ PolyarniyNikolay/Lab2/install_and_run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-sudo apt-get install python3.4 python3-pip
-pip3 install -e /vagrant/
+sudo apt-get install -y python3.4 python3-pip
+sudo pip3 install -e /vagrant/
 python3 /vagrant/run_pi_node.py
