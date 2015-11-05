3767d9b538503e81e9e4200e24bc63861c7a8c7b
Vladimir Skipor <vladimirskipor@gmail.com>
2015-11-05 23:19:36 +0300

    run fix

--- PolyarniyNikolay/Lab2/install.sh
+++ PolyarniyNikolay/Lab2/install.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
+
+sudo apt-get install -y libyaml-cpp-dev
+sudo apt-get install -y htop
 sudo apt-get install -y python3.4 python3-pip
+pip3 install PyYAML==3.11
 sudo pip3 install -e /vagrant/
